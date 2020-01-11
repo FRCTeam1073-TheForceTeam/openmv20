99186f9263a4f27c461e37240c660c08b5611625
Nick Murphy <44452167+NickisNick123@users.noreply.github.com>
2020-01-11 14:05:29 -0500

    Simple Power Cell Identifier
    tracks one, nearby power-cell. tested in the MPR
    
    Co-Authored-By: kcjesse02 <kcjesse02@users.noreply.github.com>

---
+++ examples/single_color_rgb565_blob_tracking_3.py
@@ -0,0 +1,43 @@
+# Single Color RGB565 Blob Tracking Example
+#
+# This example shows off single color RGB565 tracking using the OpenMV Cam.
+
+import sensor, image, time, math
+
+
+#0 for power cell
+# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
+# The below thresholds track in general red/green/blue things. You may wish to tune them...
+thresholds = [(50, 90, -35, 35, 60, 95)]
+#generic_yellow_ball_thresholds
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(False) # must be turned off for color tracking
+sensor.set_saturation(2)
+clock = time.clock()
+
+# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
+# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
+# camera resolution. "merge=True" merges all overlapping blobs in the image.
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    for blob in img.find_blobs(thresholds, pixels_threshold=200, area_threshold=200, merge=True):
+        # These values depend on the blob not being circular - otherwise they will be shaky.
+        if blob.elongation() > 0.5:
+            img.draw_edges(blob.min_corners(), color=(255,0,0))
+            img.draw_line(blob.major_axis_line(), color=(0,255,0))
+            img.draw_line(blob.minor_axis_line(), color=(0,0,255))
+        # These values are stable all the time.
+        img.draw_rectangle(blob.rect())
+        #if blob.h*0.95 > blob.w and blob.h*1.05 < blob.w:
+            #continue
+        img.draw_cross(blob.cx(), blob.cy())
+        # Note - the blob rotation is unique to 0-180 only.
+        img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
+    print(clock.fps())
