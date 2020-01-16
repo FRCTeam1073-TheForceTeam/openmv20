8d920e8ae90597ab690307d0886243d1a4caac28
Nick Murphy <44452167+NickisNick123@users.noreply.github.com>
2020-01-15 19:34:15 -0500

    Minor chamges to blob tracking

--- examples/single_color_rgb565_blob_tracking_3.py
+++ examples/single_color_rgb565_blob_tracking_3.py
@@ -5,7 +5,7 @@
 import sensor, image, time, math
 
 
-#0 for power cell
+#0 for yellow_ball or power_cell, it doesn't matter
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green/blue things. You may wish to tune them...
 thresholds = [(45, 90, -35, 35, 60, 95)]
@@ -35,8 +35,8 @@ while(True):
             img.draw_line(blob.minor_axis_line(), color=(0,0,255))
         # These values are stable all the time.
         img.draw_rectangle(blob.rect())
-        #if blob.h*0.95 > blob.w and blob.h*1.05 < blob.w:
-            #continue
+        if blob.h*0.95 > blob.w and blob.h*1.05 < blob.w:
+            continue
         img.draw_cross(blob.cx(), blob.cy())
         # Note - the blob rotation is unique to 0-180 only.
         img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
