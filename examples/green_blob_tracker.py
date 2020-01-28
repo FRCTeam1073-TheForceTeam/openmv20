eafdae4591faf640eee28af9998a764a68e6dcb1
Linkd-Aether <jackmcevoy15@gmail.com>
2020-01-27 19:33:38 -0500

    Displays green rectangles + rudimentary distance
    Sorry I didn't push this yet, but it's here and ready to be retrofitted with CAN communication

--- examples/green_blob_tracker.py
+++ examples/green_blob_tracker.py
@@ -3,11 +3,12 @@
 import sensor, image, time, math, pyb
 from pyb import CAN
 
-threshold_index = 0
+threshold_index = 2
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 thresholds = [(80, 95, -55, -40, -20, 10), # specific_green_threshold @ 900 exposure & 9 volts
-             (15, 40, -40, -30, 20, 40)] # specific_green_threshold @ 300 exposure
+             (15, 95, -40, 0, -10, 40),
+             (50, 95, -80, -30, -10, 50)]
 
 # Camera settings
 sensor.reset()
@@ -23,22 +24,69 @@ clock = time.clock()
 red_led  = pyb.LED(1)
 green_led = pyb.LED(2)
 
-# Basic CAN setup
-can = CAN(2, CAN.NORMAL)
-can.init(CAN.NORMAL, prescaler=3,  sjw=1, bs1=11, bs2=5)  # 1000Kbps
-can.restart()
+def arbitration_id(devtype, mfr, devid, apiid):
+    retval = (devtype & 0x1f) << 24
+    retval = retval | (mfr & 0xff) << 16
+    retval = retval | (apiid & 0x3ff) << 6
+    retval = retval | (devid & 0x3f)
+    return retval
+
+def findLength(blobData):
+    return math.sqrt(math.pow(blobData[0] - blobData[2], 2) + math.pow(blobData[1] - blobData[3], 2))
+
+# CAN setup
+#can = CAN(2, CAN.NORMAL)
+#can.init(CAN.NORMAL, extframe=True, prescaler=4,  sjw=1, bs1=8, bs2=3) # 1000Kbps H7
+#range_lo = arbitration_id(12,170,3,5)
+#range_hi = arbitration_id(12,170,3,10)
+#print("ARB_LO 0x%X"%range_lo)
+#print("ARB_HI 0x%X"%range_hi)
+
+#can.setfilter(0, CAN.RANGE, 0, (range_lo, range_hi))
+#can.restart()
+
+#data1 = bytearray(b'HelloRio')
+#data2 = bytearray(b'Infinite')
+#counter = 0
+
+#canbuf = bytearray(8)
+#canlist = [0,0,0,memoryview(canbuf)]
+
+#mysendid = arbitration_id(12, 170, 3, 4)
 
 while(True):
     numBlobs = 0
     clock.tick()
     img = sensor.snapshot()
-    for blob in img.find_blobs([thresholds[threshold_index]], pixels_threshold=200, area_threshold=150, merge=True):
-        numBlobs+=1
-        # Below variables are the x and y coordinates of the inner port target
-        target_x = int((blob.major_axis_line()[0] + blob.major_axis_line()[2]) / 2)
-        target_y = int(min(blob.minor_axis_line()[1], blob.minor_axis_line()[3]))
-        img.draw_circle(target_x, target_y, 5)
-        img.draw_rectangle(blob.rect())
+    for blob in img.find_blobs([thresholds[threshold_index]], pixels_threshold=100, area_threshold=1000, merge=True):
+        # print(findLength(blob.minor_axis_line())) = 90
+        # print(findLength(blob.major_axis_line())) = 185
+        if findLength(blob.minor_axis_line()) > findLength(blob.major_axis_line())/3 and findLength(blob.minor_axis_line()) < findLength(blob.major_axis_line())/1.8:
+            numBlobs += 1
+            # Below variables are the x and y coordinates of the inner port target
+            target_x = int((blob.major_axis_line()[0] + blob.major_axis_line()[2]) / 2)
+            target_y = int(min(blob.minor_axis_line()[1], blob.minor_axis_line()[3]))
+            img.draw_circle(target_x, target_y, 5)
+            img.draw_rectangle(blob.rect())
+            # dims: 37.75 x 18
+            # dist for full x on full screen: 320 pixels, 31.5 inches
+            # dist for full x on half screen: 160 pixels, 72 inches
+            # dist for full x on 4/5 screen: 256 pixels, 40 inches
+            # width = 320
+            dist = (0.001253 * (findLength(blob.major_axis_line()) ** 2)) - (0.8547 * findLength(blob.major_axis_line())) + 176.7
+            print("distance to target is: " + str(dist))
+
+#    try:
+#        if counter == 0:
+#            can.send(data1, mysendid, timeout=33)
+#            counter = counter + 1
+#        else:
+#            can.send(data2, mysendid, timeout=33)
+#            counter = 0
+#    except:
+#        print('CANBus exception!')
+#        can.restart()
+#        pass
 
     # Green LED = at least 1 blob seen, red LED = no blobs seen
     if numBlobs > 0:
@@ -47,3 +95,9 @@ while(True):
     else:
         green_led.off()
         red_led.on()
+#    time.sleep(100)
+
+    # Check for data from CAN:
+#    if can.any(0):
+#        can.recv(0, canlist, timeout=50)
+#        print('!')
