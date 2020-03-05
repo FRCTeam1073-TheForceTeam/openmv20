4ba5dc7cf242ec7b21143c1889d007e2a89cd8b5
Katherine Jesse <kcjesse02@gmail.com>
2020-03-04 22:05:02 -0500

    basic data analysis program
    file used in frame_count should be on the vision SD card

--- WeekZeroExamples/untitled_1.py
+++ WeekZeroExamples/untitled_1.py
@@ -1,11 +1,10 @@
 # Untitled - By: katherine - Sat Feb 22 2020
 import sensor, image, time
-def list_creator(path):
-    frame_list=[]
-    video = image.ImageReader(path);
-    while(video.next_frame(copy_to_fb=False, loop=False) != None):
-        frame_list.append(video.next_frame())
-    return frame_list
+def file_reader(path):
+    frame_number = 0
+    video = image.ImageReader(path)
+    while(video.next_frame(copy_to_fb = False, loop = False)):
 
-images = list_creator('/Users/katherine/Documents/GitHub/openmv20/WeekZeroExamples/Week0Videos/GOODMatch0.mjpeg')
-print("images saved!")
+
+
+file_reader("WeekZeroVideos/testFile.bin")
